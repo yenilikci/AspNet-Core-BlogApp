8217ed55024f3f410ec2292573e46b186464fbf6
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-12 17:20:28 +0300

    Entity Sınıfları Yazıldı

---
+++ BlogApp.Entity/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.Entity
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public string Image { get; set; }
+        public DateTime Date { get; set; }
+        public bool isApproved { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
