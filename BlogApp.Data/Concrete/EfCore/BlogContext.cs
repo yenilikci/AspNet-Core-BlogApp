d440b37f93ce182e98a660b142b9d11d6fe73ec6
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-12 18:50:30 +0300

    Veritabanı Ayarları

---
+++ BlogApp.Data/Concrete/EfCore/BlogContext.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.Data.Concrete.EfCore
+{
+    public class BlogContext : DbContext
+    {
+        //kurucu oluşturup connection string
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
+
+        //entitylerden DbSetlerimi oluşturdum
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
