c4a205537edc2d24ce65d26746a10fca69f7ecad
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-13 17:14:19 +0300

    CategoryController ve Category Listesi Sayfası

---
+++ BlogApp.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository repository;
+
+        public CategoryController(ICategoryRepository _repo)
+        {
+            repository = _repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(repository.GetAll());
+        }
+      
+
+
+
+    }
+
+}
