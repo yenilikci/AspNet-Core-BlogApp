eac6de51d0ef0ab60ddc640e4955adcff9b31192
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-13 16:51:51 +0300

    BlogController ve Blog Listesi Sayfası

---
+++ BlogApp.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IBlogRepository repository;
+
+        public BlogController(IBlogRepository _repo)
+        {
+            repository = _repo;
+        }
+            
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            //repository.GetAll() ile bir blog listesi gönderiyoruz
+            return View(repository.GetAll());
+        }
+
+    }
+}
