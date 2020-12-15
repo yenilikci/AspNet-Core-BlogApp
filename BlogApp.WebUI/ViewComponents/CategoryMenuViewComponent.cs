7fb5173af8efd42cc6253ef26874ba2ff51ada1c
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-15 17:04:23 +0300

    Category Component Tamamlandı

---
+++ BlogApp.WebUI/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.WebUI.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private ICategoryRepository _repository;
+
+        public CategoryMenuViewComponent(ICategoryRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //bütün kategori bilgilerini view üzerine taşıdık
+            return View(_repository.GetAll());
+        }
+    }
+}
